6485bd38e6ccd55d6c98902ba2fa3c14fbbb7688
MichaelHendra <michaelhendranata01@gmail.com>
2023-11-30 10:57:43 +0700

    (+) Kategori CRUD

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Validator;
 
 class KategoriController extends Controller
 {
-    //
+    public function index(){
+        $kategori = Kategori::all();
+        return view('kategori/kategori', ['kategori' => $kategori]);
+    } 
+
+    public function Tambah()  {
+        return view('kategori/kategoriTambah');
+    }
+
+    public function Create(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'id_kategori' => 'required|unique:kategoris',
+            'nama' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return redirect('/kategori/tambah')->withErrors($validator)->withInput();
+        }
+        Kategori::create([
+            'id_kategori' => $request->id_kategori,
+            'nama_kategori' => $request->nama,
+        ]);
+        return redirect('/kategori');
+    }
+
+    public function Rubah($id)  {
+        $kategori = Kategori::where('id_kategori','=',$id)->first();
+
+        return view('kategori/kategoriEdit',['kategori' => $kategori]);
+    }
+    public function Edit($id, Request $request) {
+        $kategori = Kategori::where('id_kategori', $id)->first();
+
+        // dd($kategori);
+        $validator = Validator::make($request->all(),[
+            'nama' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return redirect('/kategori/edit')->withErrors($validator)->withInput();
+        }
+        $kategori->update([
+            'nama_kategori'=>$request->nama
+        ]);
+        return redirect('/kategori');
+    }
+    public function Delete($id)  {
+        $kategori = Kategori::where('id_kategori',$id)->first();
+        $kategori->delete();
+        return redirect('/kategori');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('template');
 });
+
+Route::get('/kategori',[KategoriController::class, 'index']);
+Route::get('/kategori/tambah',[KategoriController::class,'Tambah']);
+Route::post('/kategori/tambah',[KategoriController::class,'Create']);
+Route::get('/kategori/edit/{id}',[KategoriController::class,'Rubah']);
+Route::put('/kategori/edit/{id}',[KategoriController::class, 'Edit']);
+Route::get('/kategori/delete/{id}',[KategoriController::class, 'Delete']);
